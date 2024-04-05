3790cf22d8a8c04e30f8aa92b39d59217a419a28
varun <varunsavaliya000@gmail.com>
2024-04-05 23:17:01 +0530

    added console for db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     if (connection) console.log(`connected to mongoDB: ${connection.host}`);
   } catch (error) {
+    console.log(error.message);
     process.exit(1);
   }
 };
